8c61e488be6a27bae57b98e094c0c2580ca428be
Rizvee-Hassan-Prito <rhprizveehassanpri@gmail.com>
2025-04-13 23:06:13 +0600

    Add files via upload

---
+++ SmallFishPy_ParcentageCalculate.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 13 22:59:30 2025
+
+@author: User
+"""
+
+def polygon_area(points):
+    """
+    Calculate the area of a polygon using the shoelace formula.
+    """
+    x = [p[0] for p in points]
+    y = [p[1] for p in points]
+    return 0.5 * abs(sum(x[i] * y[i+1] - x[i+1] * y[i] for i in range(-1, len(points)-1)))
+
+
+ant = {
+    "boxes": [
+        {
+            "points": [
+                [...][...],
+                [...][...],
+                ...
+            ]
+        }
+    ],
+    "height": ...,
+    "width": ...
+}
+
+image_area = ant["height"] * ant["width"]
+
+polygon_pnts = ant["boxes"][0]["points"]
+
+obj_area = polygon_area(polygon_pnts)
+
+parcent = (obj_area / image_area) * 100
+
+print(f"Object covers approximately {parcent:.2f}% of the image.")
